d6be3328c3019a002dd5846771716e2b60edc5aa
AishwaryaKandasamy <15aish98@gmail.com>
2025-09-08 13:44:06 -0500

    Lesson 4 Conditional Rendering

---
+++ my-react-app/src/Lesson-5 Conditional Rendering/AppLesson5.jsx
@@ -0,0 +1,12 @@
+import UserGreeting from "./UserGreeting"
+import './index.css';
+
+function App() {
+  return(
+    <>
+    <UserGreeting isLoggedIn = {true} username = "Aishwarya"/>
+    </>
+  )
+}
+
+export default App
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppLesson4 from './Lesson-4 Props/AppLesson4.jsx'
+import AppLesson5 from './Lesson-5 Conditional Rendering/AppLesson5.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppLesson4 />
+    <AppLesson5 />
   </StrictMode>,
 )
