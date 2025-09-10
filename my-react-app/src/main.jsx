40073f0d574ad7423550477210dda8964be20a4e
AishwaryaKandasamy <15aish98@gmail.com>
2025-09-10 07:55:58 -0500

    Lesson-11 Updater Functions

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppLesson10 from './Lesson-10 Color Picker App/AppLesson10.jsx'
+import AppLesson11 from './Lesson-11 Updater Function/AppLesson11.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppLesson10/>
-  </StrictMode>,
+    <AppLesson11/>
+  </StrictMode>
 )
